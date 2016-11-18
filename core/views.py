6714d6eabde55e53c1527fedab34204f14f33261
Arian Bakhtiarnia <arian@cafebazaar.ir>
2016-11-18 14:31:00 +0330

    Add elements to nav header

--- core/views.py
+++ core/views.py
@@ -1,5 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'index.html', {})
+
+
+def login(request):
+    pass  # TODO
+
+
+@login_required
+def logout(request):
+    pass  # TODO
+
+
+@login_required
+def profile(request):
+    pass  # TODO
--- shahna/urls.py
+++ shahna/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from core.views import home
+from core.views import home, login, logout, profile
 
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^profile/$', profile, name='profile'),
     url(r'^admin/', admin.site.urls),
 ]
