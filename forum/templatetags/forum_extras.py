fd9d42919492dc68f5304a89e51ee9b9cb4c8f56
Arian Bakhtiarnia <arian@cafebazaar.ir>
2017-06-02 16:54:21 +0430

    Change view of question lists

--- forum/templatetags/forum_extras.py
+++ forum/templatetags/forum_extras.py
@@ -11,3 +11,15 @@ def persian_number(value):
         return unichr(int_value + 1776)
     except ValueError:
         return value
+
+
+@register.filter(name='rtl_list')
+def rtl_list(input_list):
+    output_list = []
+    for i in range(0, len(input_list) / 2):
+        output_list.append(input_list[i * 2 + 1])
+        output_list.append(input_list[i * 2])
+    if len(input_list) % 2 == 1:
+        output_list.append(None)
+        output_list.append(input_list[-1])
+    return output_list
